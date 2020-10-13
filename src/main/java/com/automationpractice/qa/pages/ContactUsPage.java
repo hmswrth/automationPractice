629dc0ada9a6dd842fd1b8f2e30c9828ad3e2668
Hemanth Mudra <mudra.hemanth@gmail.com>
2020-10-14 00:04:33 +0530

    updated contact us page

---
+++ src/main/java/com/automationpractice/qa/pages/ContactUsPage.java
@@ -0,0 +1,66 @@
+package com.automationpractice.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import com.automationpractice.qa.base.Main;
+
+public class ContactUsPage extends Main{
+	// Object Repository
+	@FindBy(xpath="//select")
+	WebElement selectBox;
+	
+	@FindBy(xpath="//input[@id='email']")
+	WebElement email;
+	
+	@FindBy(xpath="//input[@id='id_order']")
+	WebElement orderID;
+	
+	@FindBy(xpath="//input[@id='fileUpload']")
+	WebElement fileUpload;
+	
+	@FindBy(xpath="//textarea[@id='message']")
+	WebElement message;
+	
+	@FindBy(xpath="//button[@id='submitMessage']")
+	WebElement submitBtn;
+	
+	@FindBy(xpath="//*[@id='center_column']/p")
+	WebElement successMessage;
+	
+	public ContactUsPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void selectSubjectHeading() {
+		Select select = new Select(selectBox);
+		select.selectByValue("2");
+	}
+	
+	public void enterEmail(String testData) {
+		email.sendKeys(testData);
+	}
+	
+	public void enterOrderID(String testData) {
+		orderID.sendKeys(testData);
+	}
+	
+	public void uploadFile() {
+		fileUpload.sendKeys(System.getProperty("user.dir")+"/com/automationpractice/qa/testdata/peterpan.jpg" );
+	}
+	
+	public void enterMessage(String testData) {
+		message.sendKeys(testData);
+	}
+	
+	public void submitButton() {
+		submitBtn.click();
+	}
+	
+	public String verifySentMessage() {
+		
+		return successMessage.getText();
+	}
+}
--- src/main/java/com/automationpractice/qa/pages/HomePage.java
+++ src/main/java/com/automationpractice/qa/pages/HomePage.java
@@ -1,7 +1,36 @@
 package com.automationpractice.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.automationpractice.qa.base.Main;
 
 public class HomePage extends Main {
+	//object repository
+	
+	@FindBy(xpath="//a[@title='Contact Us']")
+	WebElement contactUsBtn;
+	
+	@FindBy(xpath="//input[@id='search_query_top']")
+	WebElement searchBox;
+	
+	@FindBy(xpath="//button[@name='submit_search']")
+	WebElement searchBtn;
+	
+	public HomePage() { //initializing object rep
+		PageFactory.initElements(driver, this);
+	}
+	
+	public ContactUsPage contactUs() {
+		contactUsBtn.click();
+		return new ContactUsPage();
+	}
+	
+	public SearchResultsPage search(String searchData) {
+		searchBox.sendKeys(searchData);
+		searchBtn.click();
+		return new SearchResultsPage();
+	}
 
 }
