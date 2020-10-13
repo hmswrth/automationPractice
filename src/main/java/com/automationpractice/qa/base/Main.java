b080bdaf05694f590710af4e7cc65c3a1bb92457
Hemanth Mudra <mudra.hemanth@gmail.com>
2020-10-13 23:00:09 +0530

    updated base class and added a config file

--- src/main/java/com/automationpractice/qa/base/Main.java
+++ src/main/java/com/automationpractice/qa/base/Main.java
@@ -1,5 +1,47 @@
 package com.automationpractice.qa.base;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+import com.automationpractice.qa.utils.BrowserSetup;
+
 public class Main {
+	public static WebDriver driver;
+	public static Properties prop;
+	public static JavascriptExecutor js;
+	public static long PAGE_LOAD_TIMEOUT = 20;
+	public static long IMPLICIT_WAIT = 10;
+	
+	public Main() {
+		
+		try {
+			//loading from config file
+			prop = new Properties();
+			FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"/src/main/java/com/automationpractice/qa/config/config.properties");
+			prop.load(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void init() {
+		driver = BrowserSetup.setBrower(); //instantiate webdriver specified in the config file
+		driver.manage().window().maximize(); //maximize the windows	
+		driver.manage().deleteAllCookies(); //clear cookies
+		
+		driver.manage().timeouts().pageLoadTimeout(PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS); //wait for the page to load
+		driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS); 
+		
+		driver.get(prop.getProperty("url")); //get url from the config file and pass it to the browser 
+		
+		js = (JavascriptExecutor) driver; //instantiate js executor which can be used later
+		
+	}
 
 }
--- src/main/java/com/automationpractice/qa/utils/BrowserSetup.java
+++ src/main/java/com/automationpractice/qa/utils/BrowserSetup.java
@@ -3,12 +3,16 @@ package com.automationpractice.qa.utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BrowserSetup {
+import com.automationpractice.qa.base.Main;
+
+public class BrowserSetup extends Main {
 	static String driverPath = System.getProperty("user.dir") + "/Drivers/";
 
-	public static WebDriver setChromeDriver() {
-		System.setProperty("webdriver.chrome.driver", driverPath + "chormedriver.exe");
-		WebDriver driver = new ChromeDriver();
+	public static WebDriver setBrower() {
+		if (prop.getProperty("browser") == "chrome") {
+			System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
+			driver = new ChromeDriver();
+		}
 		return driver;
 
 	}
